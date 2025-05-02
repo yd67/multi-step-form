a535791ea07b3048a82a775f3a22c30c1969bc81
Darius <yvens67@hotmail.fr>
2025-05-03 00:00:50 +0200

    structure et center ma div

--- src/components/MultiStepForm.vue
+++ src/components/MultiStepForm.vue
@@ -1,7 +1,16 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-    <h1 class="text-3xl text-marine-blue ">Test Multi step Form</h1>
-    
+  <section
+    class="md:flex w-screen md:h-screen items-center justify-center bg-magnolia"
+  >
+    <div
+      class="md:w-[90%] lg:w-[80%] xl:w-[70%] md:bg-white-t rounded-xl md:shadow-md md:p-4 md:flex justify-center"
+    >
+      <div class="md:w-[30%] h-full"></div>
+      <div
+        class="md:w-[70%] rounded-xl md:rounded-none bg-white-t md:px-10 lg:px-15 py-5 md:pt-10 -mt-20 md:-mt-0 mx-5 px-6"
+      ></div>
+    </div>
+  </section>
 </template>
