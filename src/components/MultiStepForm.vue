8d6ea21d4ccea226b2ba03b23021785b15a30230
Darius <yvens67@hotmail.fr>
2025-05-03 00:34:57 +0200

    creation et mise en forme  du composant de suivis d'etape(FormNav.vue)

--- src/components/MultiStepForm.vue
+++ src/components/MultiStepForm.vue
@@ -1,4 +1,10 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import FormNav from "./FormNav.vue";
+
+const currentStep = ref(1)
+
+</script>
 
 <template>
   <section
@@ -7,10 +13,18 @@
     <div
       class="md:w-[90%] lg:w-[80%] xl:w-[70%] md:bg-white-t rounded-xl md:shadow-md md:p-4 md:flex justify-center"
     >
-      <div class="md:w-[30%] h-full"></div>
+      <div class="md:w-[30%] h-full">
+        <FormNav></FormNav>
+      </div>
       <div
         class="md:w-[70%] rounded-xl md:rounded-none bg-white-t md:px-10 lg:px-15 py-5 md:pt-10 -mt-20 md:-mt-0 mx-5 px-6"
-      ></div>
+        >
+        <h1 class="text-3xl text-marine-blue ">Test Multi step Form</h1>
+        <p class="mt-8"> Lorem ipsum dolor sit amet consectetur adipisicing elit.sunt voluptatibus 
+            adipisci quia aliquam quisquam ab laboriosam,deleniti minima autem harum 
+            libero dicta doloremque exercitationem ea numquam. Corrupti, facilis?
+         </p>
+    </div>
     </div>
   </section>
 </template>
---
+++ src/composables/useIsMobile.vue
@@ -0,0 +1,23 @@
+<script>
+import { ref, computed, onMounted, onUnmounted } from "vue";
+
+export function useIsMobile() {
+  const screenWidth = ref(window.innerWidth);
+
+  const change = () => {
+    screenWidth.value = window.innerWidth;
+  };
+
+  onMounted(() => {
+    window.addEventListener("resize", change);
+  });
+  
+  onUnmounted(() => {
+    window.removeEventListener("resize", change);
+  });
+
+  const isMobile = computed(() => screenWidth.value < 767);
+
+  return { isMobile };
+}
+</script>
