8be95f174440a2baac6a91c27723071bc48ee51c
Darius <yvens67@hotmail.fr>
2025-05-02 23:50:48 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import MultiStepForm from "./components/MultiStepForm.vue";
+</script>
+
+<template>
+  <MultiStepForm></MultiStepForm>
+</template>
+
+<style scoped></style>
---
+++ src/components/MultiStepForm.vue
@@ -0,0 +1,7 @@
+<script setup>
+</script>
+
+<template>
+    <h1 class="text-3xl text-marine-blue ">Test Multi step Form</h1>
+    
+</template>
